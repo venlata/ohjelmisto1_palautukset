82f3bbbca1ab6161dbceafcf26f47d2bd8d2c4af
Venla Tapio <venlatapio@outlook.com>
2026-08-30 12:58:30 +0300

    peliprojekti lisätty, mod03

---
+++ peliprojekti/ePeli.py
@@ -0,0 +1,3 @@
+nimi = input("Mikä on nimesi?\n")
+ika = input("Mikä on ikäsi?\n")
+print(f"Hei, {nimi}, {ika}v.")
---
+++ peliprojekti/readme.md
@@ -0,0 +1,3 @@
+# ePeli
+Venla
+Aloitettu 30.8.2026; ohjelma kysyy pelaajan nimen ja iän ja tervehtii häntä.
