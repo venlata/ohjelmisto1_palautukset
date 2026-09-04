64ab4d71c195f04123c60089b567e6eb75f62c2c
Venla Tapio <venlatapio@outlook.com>
2026-09-04 11:03:49 +0300

    välipush tunnin aikana

--- mod05/tehtava5.py
+++ mod05/tehtava5.py
@@ -1,81 +1,79 @@
 #Tehtävä 5 / mod05
 #Tehtävä 5, osa 1:
 import random
-def t5o1():
-    lukuni = 0
-    while lukuni <= 1000:
-        print(lukuni)
-        lukuni += 3
+
+lukuni = 0
+while lukuni <= 1000:
+    print(lukuni)
+    lukuni += 3
 
 #Tehtävä 5, osa 2
-def t5o2():
-    tuuma = 2.54
+tuuma = 2.54
+vastahus = float(input("Anna tuumat niin käännetään senttimetreiksi: "))
+while vastahus > 0:
+    print(f"{(vastahus) * (tuuma):.2f} senttimetreissä")
     vastahus = float(input("Anna tuumat niin käännetään senttimetreiksi: "))
-    while vastahus > 0:
-        print(f"{(vastahus) * (tuuma):.2f} senttimetreissä")
-        vastahus = float(input("Anna tuumat niin käännetään senttimetreiksi: "))
-    else:
-        print("Älä anna negatiivisia lukuja :()")
+else:
+    print("Älä anna negatiivisia lukuja :()")
 
 
 #Tehtävä 5, osa 3
-def t5o3():
-    luvut = []
-    pienin = None
-    isoin = None
-    while True:
-        luku = input("Syötä luku: ")
-        if luku == "":
-            break
-        luku = int(luku)
-        luvut.append(luku)
-        if pienin is None or luku < pienin:
-            pienin = luku
-        if isoin is None or luku > isoin:
-            isoin = luku
-    print(f"Tässä pienin luku: {pienin}\nTässä isoin luku: {isoin}")
+luvut = []
+pienin = None
+isoin = None
+while True:
+    luku = input("Syötä luku: ")
+    if luku == "":
+        break
+    luku = int(luku)
+    luvut.append(luku)
+    if pienin is None or luku < pienin:
+        pienin = luku
+    if isoin is None or luku > isoin:
+        isoin = luku
+print(f"Tässä pienin luku: {pienin}\nTässä isoin luku: {isoin}")
 
 #Luulin pitkään ettei tää toimi ja lisäsin ja poistin osioita koska unohin lisätä t5o3 sulut :D 
 #Kunnes kokeilin Thonnyn kautta myös
 
 #Tehtävä 5 osa 4
-def t5o4():
 
-    kokluku = random.randint(1, 10)
 
-    while True:
-        arvaus = input("Arvaa luku: ")
-        arvaus = int(arvaus)
+kokluku = random.randint(1, 10)
 
-        if arvaus == kokluku:
-            print("Oikein")
-            break
+while True:
+    arvaus = input("Arvaa luku: ")
+    arvaus = int(arvaus)
 
-        if arvaus < kokluku:
-            print("Liian pieni arvaus")
+    if arvaus == kokluku:
+        print("Oikein")
+        break
 
-        if arvaus > kokluku:
-            print("Liian suuri arvaus")
+    if arvaus < kokluku:
+        print("Liian pieni arvaus")
+
+    if arvaus > kokluku:
+        print("Liian suuri arvaus")
 
 
 #Tehtävä 5 osa 5
-def t5o5():
-    
-    tunnari = "python"
-    passu = "rules"
-    maximit = 5
-    yritykset = 0
 
-    while yritykset <= maximit:
-    #for yritys in range(0, maximit):
 
-        stunnus = input("Syötä käyttäjätunnus: ")
-        spassu = input("Syötä salasana: ")
-        yritykset += 1
-        if stunnus == tunnari and spassu == passu:
-            print("Tervetuloa")
-            break
-        if yritykset >= maximit:
-            print("Pääsy evätty")
-            break
+tunnari = "python"
+passu = "rules"
+maximit = 5
+yritykset = 0
+
+while yritykset <= maximit:
+#for yritys in range(0, maximit):
+
+    stunnus = input("Syötä käyttäjätunnus: ")
+    spassu = input("Syötä salasana: ")
+    yritykset += 1
+    if stunnus == tunnari and spassu == passu:
+        print("Tervetuloa")
+        break
+    if yritykset >= maximit:
+        print("Pääsy evätty")
+        break
 
--- peliprojekti/ePeli.py
+++ peliprojekti/ePeli.py
@@ -1,38 +1,29 @@
 
-def p1o1():   
-     #Ikä, nimi ja tervehdys lisätty peliprojektin ekassa tehtävässä
+#Ikä, nimi ja tervehdys lisätty peliprojektin ekassa tehtävässä
 
-    nimi1 = input("Mikä on nimesi?\n")
-    ika = int(input("Mikä on ikäsi?\n"))
-    #Ikälukko lisätty tehtävässä peliprojekti 2 2.9.2026. 
-    #Tervehdys siirtyy lukon jälkeen, pieniä lapsia ei tervehditä.
+nimi1 = input("Mikä on nimesi?\n")
+ika = int(input("Mikä on ikäsi?\n"))
+#Ikälukko lisätty tehtävässä peliprojekti 2 2.9.2026. 
+#Tervehdys siirtyy lukon jälkeen, pieniä lapsia ei tervehditä.
 
-    while True:
-        if ika < 12:
-            print("Tänne ei lapsia haluta! Hyvästi!")
-            break
-        if ika >= 12:
-            print(f"Hei {nimi1}, {ika}v. Tervetuloa ePeliin!\n")
-            while ika >= 12:
-                print("\033[1;31m(っ◕‿◕)っ PÄÄVALIKKO\n")
-
-                komento = input("Anna komento: \033[0m")
-                print("\n")
-
-                if komento == "lopeta":
-                    break
-
-                elif komento == "olen iloinen":
-                    print("Erikoista!\n")
-                elif komento == ":p":
-                    print("(◕‿-)\n")
+while True:
+    if ika < 12:
+        print("Tänne ei lapsia haluta! Hyvästi!")
         break
-        
-        
-        
-               
+    if ika >= 12:
+        print(f"Hei {nimi1}, {ika}v. Tervetuloa ePeliin!\n")
+        while ika >= 12:
+            print("\033[1;31m(っ◕‿◕)っ PÄÄVALIKKO\n")
 
-        
+            komento = input("Anna komento: \033[0m")
+            print("\n")
 
+            if komento == "lopeta":
+                break
 
-p1o1()
+            elif komento == "olen iloinen":
+                print("Erikoista!\n")
+            elif komento == ":p":
+                print("(◕‿-)\n")
+    break
+
